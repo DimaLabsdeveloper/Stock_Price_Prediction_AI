33325e194e37890186c05dbb1bf7ae7f874e2cef
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2023-06-08 00:42:01 +0200

    some changes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import sys
-
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import yfinance as yf
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
+from textblob import TextBlob
 
 # Define the ticker symbol for BTC
 ticker_symbol = "BTC-USD"
@@ -47,8 +45,32 @@ for i, row in data.iterrows():
     predictions_df.loc[i, "Actual"] = row["Target"]
     predictions_df.loc[i, "Predicted"] = prediction[0]
 
+# Perform sentiment analysis on tweets
+def perform_sentiment_analysis(tweets):
+    sentiment_scores = []
+    for tweet in tweets:
+        blob = TextBlob(tweet)
+        sentiment_scores.append(blob.sentiment.polarity)
+    return sentiment_scores
+
+# Fetch Twitter data and analyze opinions
+def fetch_twitter_data():
+    # Fetch tweets (replace with your own logic to fetch tweets)
+    tweets = [
+        "BTC is doing great! #Bitcoin",
+        "I think BTC will go up in the next hour.",
+        "Avoid BTC, it's going down!",
+        "BTC price is stable.",
+        "Just bought some BTC, expecting a rise!"
+    ]
+
+    sentiment_scores = perform_sentiment_analysis(tweets)
+    average_sentiment = sum(sentiment_scores) / len(sentiment_scores)
+
+    return average_sentiment
+
 # Make future predictions
-def createfile():
+def create_file_with_predictions():
     future_predictions_df = pd.DataFrame(columns=["Date", "Close", "Target"])
     end_time_future = end_time + timedelta(hours=1)
     for j in range(24):  # Predict next 24 hours
@@ -62,8 +84,9 @@ def createfile():
         future_predictions_df.loc[end_time_future, "Target"] = future_prediction[0]
         end_time_future += timedelta(hours=1)
 
-# Save future predictions to a text file
+    # Save future predictions to a text file
     future_predictions_df.to_csv("future_predictions.txt", sep="\t", index=False)
+
 def check_buy_signal(predictions_df):
     """
     Check if it is a good moment to buy BTC based on the predictions.
@@ -88,20 +111,26 @@ def check_buy_signal(predictions_df):
     return buy_signal
 
 
-# Call the check_buy_signal function
+# Main program
+while True:
+    choice = int(input("1 - Create file with predictions\n2 - Give opinion about price\n3 - Close program\nWrite your number: "))
 
+    if choice == 1:
+        create_file_with_predictions()
+        print("File with future predictions created.\n")
 
+    elif choice == 2:
+        sentiment = fetch_twitter_data()
+        buy_signal = check_buy_signal(predictions_df)
+        if buy_signal and sentiment > 0:
+            print("It is a good moment to buy BTC.\n")
+        else:
+            print("It is not a good moment to buy BTC.\n")
 
+    elif choice == 3:
+        break
 
-choise = int(input("1 - create file with predictions\n2 - give opinion about price\n3 - close program\nWrite your number:"))
-if choise == 1:
-    createfile()
-elif choise==2:
-    buy_signal = check_buy_signal(predictions_df)
-    if buy_signal:
-        print("It is a good moment to buy BTC.")
     else:
-        print("It is not a good moment to buy BTC.")
-elif choise == 3:
-    sys.exit()
+        print("Invalid choice. Please select a valid option.\n")
 
+print("Program closed.")
